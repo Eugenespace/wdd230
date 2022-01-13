81afc98f37a5a6e97243760209ba46b265b382c8
Eugenespace <kar21016@byui.edu>
2022-01-13 09:02:47 +0000

    commit

--- js/dates.js
+++ js/dates.js
@@ -1,7 +1,7 @@
 
 const d = new Date();
 let  year = d.getFullYear();
-document.getElementById("#current_year").textContent = year ;
+document.getElementById("#current_year").innerHTML = year
 
 const lastmod = document.querySelector('#lastmod')
 lastmod.textContent = `Last updated at ${document.lastModified}`;
