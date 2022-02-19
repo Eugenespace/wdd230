7b20cbbe18356c47f1d46c8265e85d2807eb841a
Eugenespace <kar21016@byui.edu>
2022-02-19 22:34:15 +0000

    discover page initialized

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -26,3 +26,39 @@ if (dayNumber == 1 || dayNumber == 2){
 } else {
     element.classList.add("hideme");
 }
+
+
+// lazy load js//
+let lazyloadimage = document.querySelectorAll("[data-src]");
+
+const loadImg = (img) => {
+  const src = img.getAttribute("data-src");
+  img.setAttribute("src", src);
+  img.removeAttribute("data-src");
+};
+
+const observerImg = new IntersectionObserver(
+  (items, observerImg) => {
+    items.forEach((item) => {
+      if (!item.isIntersecting) return;
+      else {
+        loadImg(item.target);
+        observerImg.unobserve(item.target);
+      }
+    });
+  },
+  {
+    threshold: 1,
+    rootMargin: "0px 0px 100px 0px",
+  }
+);
+
+lazyloadimage.forEach((img) => {
+    observerImg.observe(img);
+});
+
+// displaying how many days
+
+const msInDay = 1000 * 60 * 60 * 24;
+console.log(`milliseconds in a day is ${msInDay}`);
+let difference = Math.round((today - lv)/msInDay);
--- js/dates.js
+++ js/dates.js
@@ -5,6 +5,34 @@ const lastmod = document.querySelector("#lastmod")
 lastmod.textContent = `Last updated at ${document.lastModified}`;
 document.getElementById("current_year").innerHTML = year;
 
+// lazy image js//
 
+let lazyloadimage = document.querySelectorAll("[data-src]");
+
+const loadImg = (img) => {
+  const src = img.getAttribute("data-src");
+  img.setAttribute("src", src);
+  img.removeAttribute("data-src");
+};
+
+const observerImg = new IntersectionObserver(
+  (items, observerImg) => {
+    items.forEach((item) => {
+      if (!item.isIntersecting) return;
+      else {
+        loadImg(item.target);
+        observerImg.unobserve(item.target);
+      }
+    });
+  },
+  {
+    threshold: 1,
+    rootMargin: "0px 0px 100px 0px",
+  }
+);
+
+lazyloadimage.forEach((img) => {
+    observerImg.observe(img);
+});
 
 
