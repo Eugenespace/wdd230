b015c5d332efe61618f483e446dce8555d038114
Eugenespace <kar21016@byui.edu>
2022-02-21 16:28:04 +0000

    new commit

--- chamber/js/localStoragePageVisitDate.js
+++ chamber/js/localStoragePageVisitDate.js
@@ -1,16 +1,24 @@
 
+const message = document.querySelector("#lastVisit");
+console.log(message)
+const lv = Number(localStorage.getItem("visits-ls"));
+console.log(`last visit: ${lv}`);
+const today = Date.now();
+console.log(`today is: ${today}`);
 
-// determine days between stored date and visit date ... today
-// const millisecondsToDays = 1635714636193; 
-// const lastVisit = localStorage.getItem('lastvisit');
-// const determine = (lastVisit - Date.now() / millisecondsToDays).toFixed(0)
-//  localStorage.setItem('lastvisit', Date.now());
-// console.log("Previous Page", lastVisit)
-// console.log("current Page", determine)
-
-const today = new Date;
-const lv = localStorage.getItem('lv')
-localStorage.setItem('lv',Date.now());
 const msInDay = 1000 * 60 * 60 * 24;
 console.log(`milliseconds in a day is ${msInDay}`);
+
 let difference = Math.round((today - lv)/msInDay);
+
+localStorage.setItem("visits-ls", today);
+
+if (lv !== 0){
+ 
+    message.textContent = `it's been ${difference} days since your last visit.`;
+} else {
+   
+    message.textContent = `This is your first visit enjoy surfing the page.`;
+}
+
+
