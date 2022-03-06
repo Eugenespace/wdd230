2fc74ca505fe72470b2c0bcde3479a82370171f1
Eugenespace <kar21016@byui.edu>
2022-03-06 06:40:29 +0000

    commit

---
+++ chamber/js/directory.js
@@ -0,0 +1,43 @@
+var directoryURL = './directory.json';
+fetch(directoryURL)
+.then(response =>  response.json())
+.then(jsonObject => {
+    
+    const localdirectory = jsonObject['localdirector'];
+
+    for(let i =0; i < localdirectory.length; i++){
+
+        var cards = document.createElement('section');
+        var article = document.createElement('article');
+        var image_cards = document.createElement('div');
+        
+        
+        var name = document.createElement('h5');
+        var address = document.createElement('p');
+        var desc = document.createElement('p');
+        var phone = document.createElement('p');
+        var website = document.createElement('p');
+        var img = document.createElement('img');
+
+        name.textContent = localdirectory[i].name;
+        address.textContent = localdirectory[i].address;
+        desc.textContent = localdirectory[i].desc;
+        phone.textContent = localdirectory[i].phone;
+        website.textContent = localdirectory[i].website;
+        img.setAttribute('src', localdirectory[i].imageUrl);
+
+
+        image_cards.appendChild(img, img.alt= localdirectory[i].name, img.className = 'event__image');
+
+        article.appendChild(name);
+        article.appendChild(address);
+        article.appendChild(desc);
+        article.appendChild(phone);
+        article.appendChild(website);
+
+
+        document.querySelector('div.cards').appendChild(cards, cards.appendChild(article)).appendChild(image_cards);
+        // document.querySelector('div.cards',article).appendChild(article);
+    }
+})
+.catch(err => document.querySelector('div.cards').innerHTML = err)
---
+++ chamber/js/jsnew.js
@@ -0,0 +1,4 @@
+
+var firstName = 'Ówaammwen';
+var lvdate = '2022-03-05';
+console.log(`Welcome back  ${firstName} ! You last visited on <date> ${lvdate}  </date>.`);
